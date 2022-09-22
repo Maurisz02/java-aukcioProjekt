9fa31888838793361429ad02bee534fbfa74959a
Maurisz02 <p2002adam@gmail.com>
2022-09-22 08:58:51 +0200

    lict metodus elkészült

--- src/hu/petrik/aukcioOOP/Festmeny.java
+++ src/hu/petrik/aukcioOOP/Festmeny.java
@@ -20,6 +20,57 @@ public class Festmeny {
         this.elkelt = false;
     }
 
+    public String getFesto(){
+        return this.festo;
+    }
 
+    public String getStilus(){
+        return this.stilus;
+    }
 
+    public int getLicitekSzama(){
+        return this.licitekSzama;
+    }
+
+    public int getLegmagasabbLicit(){
+        return  this.legmagasabbLicit;
+    }
+
+    public LocalDate getLegutolsoLicitIdeje(){
+        return this.legutolsoLicitIdeje;
+    }
+
+    public Boolean getElkelt() {
+        return this.elkelt;
+    }
+
+    public void setElkelt(Boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+
+    public void licit(){
+        if(!this.elkelt){
+            System.out.println("A festmény már elkelt!");
+        }else if(this.licitekSzama==0){
+            this.legmagasabbLicit = 100;
+            legmagasabbLicit++;
+            this.legmagasabbLicit =(int) Math.round(this.legmagasabbLicit/100.0) * 100;
+            this.licitekSzama++;
+            this.legutolsoLicitIdeje = LocalDate.now();
+        }else if(this.licitekSzama!=0){
+            double szazalek = this.legmagasabbLicit*0.1;
+            this.legmagasabbLicit = this.legmagasabbLicit + (int)szazalek;
+            this.legmagasabbLicit =(int) Math.round(this.legmagasabbLicit/100.0) * 100;
+            this.licitekSzama++;
+            this.legutolsoLicitIdeje = LocalDate.now();
+        }
+    }
+
+    public void licit(int merek){
+        if (merek <10 || merek > 100){
+            System.out.println("Hibás paraméter");
+        }else{
+
+        }
+    }
 }
